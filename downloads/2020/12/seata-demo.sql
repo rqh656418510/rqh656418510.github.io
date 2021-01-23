73fdb71bb8e7b259bcd379568e401da78ea7c8ee
RQH <656418510@qq.com>
2021-01-23 09:04:47 +0800

    updated by git deploy plugin

---
+++ downloads/2020/12/seata-demo.sql
@@ -0,0 +1,152 @@
+-- -------------------- Seata 库 --------------------------------
+
+DROP DATABASE IF EXISTS seata;
+CREATE DATABASE seata DEFAULT CHARACTER SET utf8;
+
+DROP TABLE IF EXISTS seata.global_table;
+CREATE TABLE seata.global_table
+(
+    `xid`                       VARCHAR(128) NOT NULL,
+    `transaction_id`            BIGINT,
+    `status`                    TINYINT      NOT NULL,
+    `application_id`            VARCHAR(32),
+    `transaction_service_group` VARCHAR(32),
+    `transaction_name`          VARCHAR(128),
+    `timeout`                   INT,
+    `begin_time`                BIGINT,
+    `application_data`          VARCHAR(2000),
+    `gmt_create`                DATETIME,
+    `gmt_modified`              DATETIME,
+    PRIMARY KEY (`xid`),
+    KEY `idx_gmt_modified_status` (`gmt_modified`, `status`),
+    KEY `idx_transaction_id` (`transaction_id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+DROP TABLE IF EXISTS seata.branch_table;
+CREATE TABLE seata.branch_table
+(
+    `branch_id`         BIGINT       NOT NULL,
+    `xid`               VARCHAR(128) NOT NULL,
+    `transaction_id`    BIGINT,
+    `resource_group_id` VARCHAR(32),
+    `resource_id`       VARCHAR(256),
+    `branch_type`       VARCHAR(8),
+    `status`            TINYINT,
+    `client_id`         VARCHAR(64),
+    `application_data`  VARCHAR(2000),
+    `gmt_create`        DATETIME(6),
+    `gmt_modified`      DATETIME(6),
+    PRIMARY KEY (`branch_id`),
+    KEY `idx_xid` (`xid`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+DROP TABLE IF EXISTS seata.lock_table;
+CREATE TABLE seata.lock_table
+(
+    `row_key`        VARCHAR(128) NOT NULL,
+    `xid`            VARCHAR(96),
+    `transaction_id` BIGINT,
+    `branch_id`      BIGINT       NOT NULL,
+    `resource_id`    VARCHAR(256),
+    `table_name`     VARCHAR(32),
+    `pk`             VARCHAR(36),
+    `gmt_create`     DATETIME,
+    `gmt_modified`   DATETIME,
+    PRIMARY KEY (`row_key`),
+    KEY `idx_branch_id` (`branch_id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+-- -------------------------------- 账户库 --------------------------------
+
+DROP DATABASE IF EXISTS seata_account;
+CREATE DATABASE seata_account DEFAULT CHARACTER SET utf8;
+
+DROP TABLE IF EXISTS seata_account.t_account;
+CREATE TABLE seata_account.t_account  (
+  `id` bigint(11) NOT NULL COMMENT 'id',
+  `user_id` bigint(11) DEFAULT NULL COMMENT '用户id',
+  `total` decimal(10, 0) DEFAULT NULL COMMENT '总额度',
+  `used` decimal(10, 0) DEFAULT NULL COMMENT '已用余额',
+  `residue` decimal(10, 0) DEFAULT NULL COMMENT '剩余可用额度',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '账户表' ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS seata_account.undo_log;
+CREATE TABLE seata_account.undo_log (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO seata_account.t_account VALUES (1, 1, 1000, 0, 1000);
+
+-- -------------------------------- 库存库 --------------------------------
+
+DROP DATABASE IF EXISTS seata_storage;
+CREATE DATABASE seata_storage DEFAULT CHARACTER SET utf8;
+
+DROP TABLE IF EXISTS seata_storage.t_storage;
+CREATE TABLE seata_storage.t_storage  (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(11) DEFAULT NULL COMMENT '产品id',
+  `total` bigint(11) DEFAULT NULL COMMENT '总库存',
+  `used` bigint(11) DEFAULT NULL COMMENT '已用库存',
+  `residue` bigint(11) DEFAULT NULL COMMENT '剩余库存',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '库存表' ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS seata_storage.undo_log;
+CREATE TABLE seata_storage.undo_log (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO seata_storage.t_storage VALUES (1, 1, 100, 0, 100);
+
+-- -------------------------------- 订单库 --------------------------------
+
+DROP DATABASE IF EXISTS seata_order;
+CREATE DATABASE seata_order DEFAULT CHARACTER SET utf8;
+
+DROP TABLE IF EXISTS seata_order.t_order;
+CREATE TABLE seata_order.t_order  (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',
+  `product_id` bigint(11) DEFAULT NULL COMMENT '产品id',
+  `count` bigint(11) DEFAULT NULL COMMENT '数量',
+  `money` decimal(11, 0) DEFAULT NULL COMMENT '金额',
+  `status` int(1) DEFAULT NULL COMMENT '订单状态:  0:创建中 1:已完结',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS seata_order.undo_log;
+CREATE TABLE seata_order.undo_log (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
