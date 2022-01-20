b7a3252068fa75bdd4d920b95acce55ddee13211
root <root@xeon-e5.lan>
2022-01-20 10:17:18 +0800

    updated by git deploy plugin

--- 404/js/analytics.js
+++ 404/js/analytics.js
@@ -1,4 +1,4 @@
-// 站点分析脚本（谷歌分析、百度统计）
+<div id="vip-container">// 站点分析脚本（谷歌分析、百度统计）
 
 "use strict";
 const host = window.location.host;
@@ -45,3 +45,25 @@ function baiduTongJi() {
   googleAnalytics();
   baiduTongJi();
 })();
+</div>
+
+			<script src="https://qiniu.techgrow.cn/js/readmore.js" type="text/javascript"></script>
+			<script>
+			var isMobile = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+			if (!isMobile) {
+				try {
+					var btw = new BTWPlugin();
+					btw.init({
+						"id": "vip-container",
+						"blogId": "16903-1366036532869-542",
+						"name": "全栈技术驿站",
+						"qrcode": "https://www.techgrow.cn/img/wx_mp_qr.png",
+						"keyword": "vip",
+						"visualHeight": "1317.5px"
+					});
+				} catch(e) {
+					console.warn(e.name + " : " + e.message);
+				}
+			}
+			</script>
+		
--- 404/js/nprogress-init.js
+++ 404/js/nprogress-init.js
@@ -1,4 +1,4 @@
-// 初始化NProgress进度条
+<div id="vip-container">// 初始化NProgress进度条
 
 "use strict";
 const page_progress = true;
@@ -10,4 +10,25 @@ $(function() {
       NProgress.done();
     });
   }
-});
+});</div>
+
+			<script src="https://qiniu.techgrow.cn/js/readmore.js" type="text/javascript"></script>
+			<script>
+			var isMobile = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+			if (!isMobile) {
+				try {
+					var btw = new BTWPlugin();
+					btw.init({
+						"id": "vip-container",
+						"blogId": "16903-1366036532869-542",
+						"name": "全栈技术驿站",
+						"qrcode": "https://www.techgrow.cn/img/wx_mp_qr.png",
+						"keyword": "vip",
+						"visualHeight": "1317.5px"
+					});
+				} catch(e) {
+					console.warn(e.name + " : " + e.message);
+				}
+			}
+			</script>
+		
